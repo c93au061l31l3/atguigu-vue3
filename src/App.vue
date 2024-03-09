158f62678e1d83f536186cc61d11c7469ad83a54
c93au061l31l3 <131161543+c93au061l31l3@users.noreply.github.com>
2024-03-09 17:40:36 +0800

    0309

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="app">
+        <h1>hello</h1>
+    </div>
+</template>
+
+<script>
+    export default{
+        name:'App' //組件名
+    }
+</script>
+
+<style>
+    .app{
+        background-color: aqua;
+    }
+</style>
